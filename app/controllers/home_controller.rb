17606c66f61239aff8cb90c6bdcff9299d54cbe4
LuBernardino <luanabernardino2009@hotmail.com>
2023-10-08 16:38:49 +0100

    add dinamic categories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ include RequestHelper
 class HomeController < ApplicationController
 
   def index
+    @listGenres = get_request("https://api.themoviedb.org/3/genre/movie/list?language=en")
     @listMovies = get_request("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1")
   end
 end
